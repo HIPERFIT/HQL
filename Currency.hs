2953773aaf4073d92829170ec1068feac647116a
Andreas Bock <bock@andreasbock.dk>
2013-11-24 16:20:15 +0100

    pricing zeros, experimenting with data types. numbers causing problems.

--- Currency.hs
+++ Currency.hs
@@ -8,11 +8,11 @@ data Cash a where
 
 instance Show (Cash a) where
   show (USD v) = "USD " ++ show v
-  show (DKK v) = "SEK " ++ show v
-  show (SEK v) = "DKK " ++ show v
+  show (DKK v) = "DKK " ++ show v
+  show (SEK v) = "SEK " ++ show v
 
-instance Num (Cash a) where
-  (USD v0) + (USD v1) = USD (v0 + v1)
+instance (Num a) => Num (Cash a) where
+  (USD v0) + (USD v1) = USD (v0+v1)
   (DKK v0) + (DKK v1) = DKK (v0+v1)
   (SEK v0) + (SEK v1) = SEK (v0+v1)
   _ + _ = error "Conversion error!" 
@@ -26,11 +26,24 @@ instance Num (Cash a) where
   signum _ = undefined
   fromInteger _ = undefined
 
-(*.) :: Cash Double -> Double -> Cash Double
-(*.) (DKK v) d = DKK $ v*d
-(*.) (SEK v) d = SEK $ v*d
-(*.) (USD v) d = USD $ v*d
-(/.) :: Cash Double -> Double -> Cash Double
-(/.) (DKK v) d = DKK $ v/d
-(/.) (SEK v) d = SEK $ v/d
-(/.) (USD v) d = USD $ v/d
+instance (Fractional a) => Fractional (Cash a) where
+  (USD v0) / (USD v1) = USD (v0/v1)
+  (DKK v0) / (DKK v1) = DKK (v0/v1)
+  (SEK v0) / (SEK v1) = SEK (v0/v1)
+  _ / _ = error "Conversion error!"
+  fromRational _ = undefined
+
+exp :: (Floating a) => Cash a -> a -> Cash a
+exp (DKK v) d = DKK $ v**d
+exp (SEK v) d = SEK $ v**d
+exp (USD v) d = USD $ v**d
+
+scale :: (Num a) => Cash a -> a -> Cash a
+scale (DKK v) d = DKK $ v*d
+scale (SEK v) d = SEK $ v*d
+scale (USD v) d = USD $ v*d
+
+add :: (Num a) => a -> Cash a -> Cash a
+add d (DKK v) = DKK $ v+d
+add d (SEK v) = SEK $ v+d
+add d (USD v) = USD $ v+d
