be4ba02203772d60c2ee3f368d2a6e08df0f052a
Johan Astborg <joastbg@gmail.com>
2013-11-28 22:31:45 +0100

    Experiments with currency support

--- Bonds2.hs
+++ Bonds2.hs
@@ -1,10 +1,101 @@
-
 --
 -- Types
 --
+
+--- Support for currency
+{-
+data Currency = USD | EUR | DKK | SEK deriving Show
+
+data Cash = Cash Double Currency deriving Show
+
+data CurrencyPair = CurrencyPair Currency Currency deriving Show
+
+data ExchangeRate = ExchangeRate Double CurrencyPair deriving Show
+-}
+
 type Date = Double -- Hack to test out implementation, will be date(s)
 
 type CouponRates = [Double]
+
+-- Some common currencies
+data Currency = USD | EUR | GBP | CHF | JPY | DKK | SEK deriving Show
+
+data Cash = Cash Double Currency | InvalidCash
+  
+-- Format cash using currency symbol
+-- TODO: Format with ***##.## decimals etc
+instance Show Cash where
+    show (Cash v USD) = "$" ++ show v
+    show (Cash v EUR) = "€" ++ show v
+    show (Cash v GBP) = "£" ++ show v
+    show (Cash v JPY) = "¥" ++ show v
+    show (Cash v CHF) = show v ++ " CHF"
+    show (Cash v DKK) = show v ++ " kr"
+    show (Cash v SEK) = show v ++ " kr"
+    show InvalidCash = "InvalidCash"
+
+instance Num Cash where
+    (Cash v USD) + (Cash w USD) = Cash (v + w) USD
+    (Cash v EUR) + (Cash w EUR) = Cash (v + w) EUR
+    (Cash v GBP) + (Cash w GBP) = Cash (v + w) GBP
+    (Cash v CHF) + (Cash w CHF) = Cash (v + w) CHF
+    (Cash v JPY) + (Cash w JPY) = Cash (v + w) JPY
+    (Cash v DKK) + (Cash w DKK) = Cash (v + w) DKK
+    (Cash v SEK) + (Cash w SEK) = Cash (v + w) SEK
+    _ + _ = InvalidCash
+    
+    (Cash v USD) * (Cash w USD) = Cash (v * w) USD
+    (Cash v EUR) * (Cash w EUR) = Cash (v * w) EUR
+    (Cash v GBP) * (Cash w GBP) = Cash (v * w) GBP
+    (Cash v CHF) * (Cash w CHF) = Cash (v * w) CHF
+    (Cash v JPY) * (Cash w JPY) = Cash (v * w) JPY
+    (Cash v DKK) * (Cash w DKK) = Cash (v * w) DKK
+    (Cash v SEK) * (Cash w SEK) = Cash (v * w) SEK
+    _ * _ = InvalidCash
+
+    -- TODOs: Ajust for negative cash?
+    fromInteger v = InvalidCash
+    abs c = c
+    signum c = c
+        
+
+data CurrencyPair = CurrencyPair Currency Currency
+
+instance Show CurrencyPair where
+    show (CurrencyPair b q) = show b ++ "/" ++ show q
+
+data ExchangeRate = ExchangeRate Double CurrencyPair
+
+-- TODO: Bid/Ask
+instance Show ExchangeRate where
+    show (ExchangeRate mid pair) = show pair ++ " " ++ show mid 
+
+{-
+
+    For example, use the Cash data
+    > Cash 115.2 USD
+    $115.2
+
+    > Cash 89.0 SEK
+    89.0 kr
+
+    Define a currecy pair
+    > CurrencyPair EUR USD
+    EUR/USD
+
+    Specify an exchange rate for EUR/USD
+    > ExchangeRate 1.32 (CurrencyPair EUR USD)
+    EUR/USD 1.21
+
+    **** TODOs
+    Support conversion of currencies, like this:
+    
+    Cash 1000.00 USD `to` EUR
+    -- Will use implicits and infix operator
+    -- Idea: use the exchange rate as an implicit (ImplicitParams)
+    -- See: http://www.haskell.org/haskellwiki/Implicit_parameters
+
+-}
 
 --type Years = Double -- Can be fractional
 --type Payment = Cash Double
