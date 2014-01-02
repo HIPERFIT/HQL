1f133b04325c2df9bdbd79b90e7a7056ffcc31a7
Johan Astborg <joastbg@gmail.com>
2014-01-02 23:02:48 +0100

    Added test groups

--- tests/Properties.hs
+++ tests/Properties.hs
@@ -1,27 +1,33 @@
 import Test.Framework (Test, defaultMain, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck (Arbitrary(..))
---import Test.QuickCheck
---import Test.QuickCheck.All
 import qualified Data.Vector as V
---import qualified Data.Attoparsec.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as L
---import qualified Data.Text as T
---import qualified Data.HashMap.Strict as H
 import Data.Time.Clock (UTCTime(..))
 import Data.Time (ZonedTime(..))
---import Instances ()
---import Types
---import Encoders
---
+
 encodeInteger :: Integer -> Bool
 encodeInteger i = (show i) == (show i)
 
 main :: IO ()
 main = defaultMain tests
 
+-- Tests goes here
+-- TODO: Update test cases...
 tests :: [Test]
 tests = [
-	testGroup "encode" [
+	testGroup "Bonds" [
+		testProperty "encodeInteger" encodeInteger
+	],
+	testGroup "Discounting" [
+		testProperty "encodeInteger" encodeInteger
+	],
+	testGroup "TermStructure" [
+		testProperty "encodeInteger" encodeInteger
+	],
+	testGroup "Calendar" [
+		testProperty "encodeInteger" encodeInteger
+	],
+	testGroup "Currency" [
 		testProperty "encodeInteger" encodeInteger
 	]]
