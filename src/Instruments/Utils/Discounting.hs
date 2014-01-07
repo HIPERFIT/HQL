8451c686c9d810b8775f2ec07fa39d7e148b3a50
Johan Astborg <joastbg@gmail.com>
2014-01-07 23:31:47 +0100

    Rebuilding of interest rates

--- src/Instruments/Utils/Discounting.hs
+++ src/Instruments/Utils/Discounting.hs
@@ -1,26 +1,106 @@
-module Instruments.Utils.Discounting where
-import Instruments.Utils.TermStructure
-import Utils.Calendar
---import Utils.Currency
-import qualified Data.Map as M
+-- |
+-- Module:      Instruments.Utils.Discounting
+-- Copyright:   (c) 2013 HIPERFIT
+-- License:     BSD-3
+-- Maintainer:  Johan Astborg <joastbg@gmail.com>
+-- Stability:   experimental
+-- Portability: portable
+--
+-- Types and functions for working with discounting
+-- and time value of money.
 
+module Instruments.Utils.Discounting where
+--import Instruments.Utils.TermStructure
+import Instruments.Utils.InterestRate
+
+--import qualified Data.Map as M
+
+{-
 -- Round a number f to n number of digits
-rnd :: Integer -> Double -> Double
+--rnd :: Integer -> Double -> Double
 rnd n f = fromInteger (round $ f * (10 ^ nn)) / (10.0 ^^ nn)
 	where nn = max 0 (n - floor(logBase 10 f) - 1)
 
 -- |Discount Factors
-df :: Double -> Rate -> Double -- DiscountFactor
-df t r = exp(-(r/100.0)*t)
+)
 
 df' t maturityT r = exp(-(r/100.0)*(maturityT - t))
 
 discountFactor' :: Double -> Double -> Double -> Double -> Double
 discountFactor' r maturityT t d = df' t maturityT r
+-}
+
+---- NEW CODE
+{-
+type AnalyticalTermStructure = Double -> Double
+
+data TermStructure = Analytical AnalyticalTermStructure
+
+instance Show TermStructure where
+    show (Analytical _) = "AnalyticalTermStructure"
+            
+-- |Create an analytic term structure
+termStructure :: AnalyticalTermStructure -> TermStructure
+termStructure = Analytical
+-}
+
+--type Points = M.Map Date ContinuousInterestRate -- Assumes zero rates
+--type InterpolatedTermStructure = Double -> ContinuousInterestRate
+type AnalyticalTermStructure = Double -> ContinuousInterestRate
+
+data TermStructure = --Interpolated InterpolatedTermStructure
+                    Analytical AnalyticalTermStructure
+
+instance Show TermStructure where
+--    show (Interpolated _) = "InterpolatedTermStructure"
+    show (Analytical _) = "AnalyticalTermStructure"
+            
+-- |Create an analytic term structure
+termStructure :: AnalyticalTermStructure -> TermStructure
+termStructure = Analytical
+
+type DiscountFactor = Double -> Double -> Double
+
+continuousDf offsetT maturityT r = exp(-(r/100.0)*(maturityT - offsetT))
+
+-- | Represents a single discount factor in continuous time
+--
+--discountFactor :: Double -> Double -> Double -> Double -> Double
+--discountFactor r maturityT t d = continuousDf t maturityT r
+
+-- | Represents a lazy list of discount factors in continuous time
+--
+
+-- Continuous discount factor p(t,T), T is fixed
+
+InterestRate
+
+interestRate -> discountFactor
+termStructure -> discountFactor
+
+discountFactor interestRate offsetT maturityT
+discountFactor termStructure offsetT maturityT
+
+discountFactors :: CompoundedInterestRate -> Double -> [Double]
+
+discountFactors interestRate offsetT = 
+  zipWith (continuousDf offsetT) [(offsetT+1-delta)..] (repeat continuousRate)
+  where continuousRate = rate (toContinuous interestRate)
+	delta = 0.0 -- TODO: Daycount conventions
+
+discountFactors' :: TermStructure -> Double -> [Double]
+discountFactors' (Analytical f) offsetT = 
+  map (\t ->continuousDf offsetT t (rate (toContinuous (f t)))) [offsetT+1-delta]
+  where delta = 0.0 -- TODO: Daycount conventions
+
+-- Forward rate
+
+-- take 5 $ discountFactors (InterestRate 10.4713 Compounded Monthly) 0
+-- take 10 $ discountFactors' (Analytical (\t -> ContinuousInterestRate 5.0)) 0.0
 
 -- |Lazy lists Discount Factors from rate, rates of term structure.
 -- will repeat the input sequence to infinity
-
+{-
 discountFactors' :: InterestRates -> Rate -> Rate -> [Rate]
 
 discountFactors' (InterestRate Continuous r) offsetT  delta=
@@ -76,3 +156,11 @@ discountFactors c now (Interpolated ts) ds =
 --tests = [head df2 == 0.9593493353414723]
 
 --testAll = all (==True) tests
+--
+--
+-}
+
+-- $use
+-- 
+-- This section contains basic information about using the
+-- discount factor and related functions defined in this module.
--- src/Instruments/Utils/InterestRate.hs
+++ src/Instruments/Utils/InterestRate.hs
@@ -1,8 +1,24 @@
-module Instruments.Utils.InterestRate where
- 
-type Rate = Double
-type DayCounter = Double
+--{-# LANGUAGE DataKinds #-}
 
+-- |
+-- Module:      Instruments.Utils.InterestRate
+-- Copyright:   (c) 2013 HIPERFIT
+-- License:     BSD-3
+-- Maintainer:  Johan Astborg <joastbg@gmail.com>
+-- Stability:   experimental
+-- Portability: portable
+--
+-- Types and functions for working with interest rates.
+
+module Instruments.Utils.InterestRate where
+
+type ContinuousRate = Double
+type CompoundedRate = Double
+type Rate = Double
+-- | Represents the compounding frequency
+-- 
+-- An interest rate can have different compounding frequencies.
+-- For other frequencies than specified, use OtherFrequency.
 data Frequency = Once -- For Zero Bond
                | Annually
                | SemiAnnually
@@ -11,42 +27,112 @@ data Frequency = Once -- For Zero Bond
 	       | NoFrequency 
                | OtherFrequency Int deriving (Show)
 
+-- | Represents the type of compounding
+--
+-- For discrete compounding, use Compounded
+-- For simple compounding, eg together with Once, use Simple
+-- Continuous compounding is the default used in HQL internally.
 data Compounding = Continuous 
 		 | Compounded 
 		 | Simple 
 		 | SimpleThenCompounded deriving (Show)
- 
-data ContinuousInterestRate = ContinuousInterestRate Rate deriving (Show)
-data CompoundedInterestRate = InterestRate Rate Compounding Frequency deriving (Show)
- 
-class InterestRate a where
-    rate 	:: a -> Rate
-    compounding :: a -> Compounding
-    frequency   :: a -> Frequency
-    toContinuous :: a -> ContinuousInterestRate 
- 
-instance InterestRate ContinuousInterestRate where
-    rate (ContinuousInterestRate r)	= r
-    compounding                         = const Continuous
-    frequency                     	= const Annually
-    toContinuous (ContinuousInterestRate r) = ContinuousInterestRate r
 
-instance InterestRate CompoundedInterestRate where
-    rate (InterestRate r _ _ ) 	= r
-    compounding (InterestRate _ c _) = c
-    frequency (InterestRate _ _ f)   = f
-    toContinuous (InterestRate r Continuous _) = ContinuousInterestRate r
-    toContinuous (InterestRate r c f) = case f of
-        Once             -> ContinuousInterestRate (mc r 1)
-	Annually         -> ContinuousInterestRate (mc r 1)
-	SemiAnnually     -> ContinuousInterestRate (mc r 2)
-  	Quarterly        -> ContinuousInterestRate (mc r 4)
-  	Monthly          -> ContinuousInterestRate (mc r 12)
-   	OtherFrequency i -> ContinuousInterestRate (mc r (fromIntegral i))
-   	_ -> ContinuousInterestRate 0.0 
-	where mc r f = f*(exp(r/(100*f)) - 1)*100
+-- | The continous compounded interest rate is used internally
+--
+-- Every interest rate other than continuous is converted using toContinuous.
+--data ContinuousInterestRate = ContinuousInterestRate Rate deriving (Show)
+--data CompoundedInterestRate = InterestRate Rate Compounding Frequency deriving (Show)
 
 
--- Helpers
---mc 1 r = log(1+r/100.0)*100.0
---mc p r = (log((1.0+r/(100.0*pp))**pp))*100 where pp = fromIntegral p
+--type Rate = Double
+type Time = Double
+type Offset = Double
+type Maturity = Double
+
+data DiscountFactor = DiscountFactor Rate Maturity deriving (Show)
+
+--InterestRateInstrument = ForwardRate | InterestRate | TermStructure
+
+data InterestRate = ShortRate Rate
+		  | ContinuousSpotRate Rate Offset Offset 
+	          | ContinuousForwardRate Rate Time Offset Offset
+		  | LIBORSpotRate Rate Offset Offset 
+	          | LIBORForwardRate Rate Time Offset Offset
+	          deriving (Show)
+
+{-
+data ContinousInterestRate = ContinuousSpotRate Offset Offset 
+			   | ContinuousForwardRate Time Offset Offset
+
+data SimpleInterestRate = SimpleSpotRate Offset Offset 
+			| SimpleForwardRate Time Offset Offset
+-}
+
+class InterestRateInstrument a where
+    rate :: a -> Rate
+    discountFactor :: a -> DiscountFactor
+
+instance InterestRateInstrument InterestRate where
+
+    -- Returns the short rate
+    rate (ShortRate r) = r
+
+    -- Returns the continuously compounded spot rate
+    rate (ContinuousSpotRate r _ _) = r
+
+    -- Returns the continuously compounded forward rate
+    rate (ContinuousForwardRate r _ _ _) = r
+
+    -- Returns the simple LIBOR spot rate
+    rate (LIBORSpotRate r _ _) = r
+
+    -- Returns the simple LIBOR forward rate
+    rate (LIBORForwardRate r _ _ _) = r
+
+    -- Convert interest rate to DiscountFactor
+    discountFactor (ShortRate r) = DiscountFactor (exp(-r/100.0)) 0
+	
+    -- Returns the discount factor p(S,T)
+    discountFactor (ContinuousSpotRate r offsetS offsetT) = DiscountFactor (exp(-r(offsetT-offsetS))) offsetT
+
+    -- Returns the continuously compounded forward rate for [S,T] contracted at t
+    discountFactor (ContinuousForwardRate r time offsetS offsetT) = DiscountFactor (exp) offsetT
+
+
+    discountFactor (LIBORSpotRate r _ offsetT) = DiscountFactor r offsetT
+    discountFactor (LIBORForwardRate r time _ offsetT) = DiscountFactor (exp(-r*(offsetT - time))) offsetT
+
+
+
+{-
+ -
+ShortRate rate
+SpotRate offsetS offsetT DiscountFactor = 
+ForwardRate t offsetS offsetT
+
+ShortRate rate
+
+
+
+
+
+
+
+
+Simple | Continous | Exponential 
+-}
+
+-- $use
+-- 
+-- This section contains basic information about using the
+-- interest rate defined in this module.
+--
+-- To create a continuously compounded rate from a annually
+-- discretely compounded rate of 5.0%:
+--
+-- > rate (toContinuous (InterestRate 5.0 Compounded Annually))
+--
+-- And another variation of the same theme:
+--
+-- > rate (toContinuous (InterestRate 10.4713 Compounded Monthly))
+
